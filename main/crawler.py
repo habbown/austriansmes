fb2a9fe5b28b3c1200c20d53bcebccbd16928480
meger <egermarcus@gmail.com>
2018-06-22 18:44:06 +0200

    transfered classes to crawler.py, optimized imports

--- main/crawler.py
+++ main/crawler.py
@@ -1,15 +1,20 @@
-import bs4
-from bs4 import BeautifulSoup
 import locale
-import re
-from main import logindata
-import requests
+import os
 import pyodbc
-from sqlalchemy import create_engine
+import re
+import time
+
+import bs4
+import numpy as np
 import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+from sqlalchemy import create_engine
 from tqdm import tqdm
-from .settings import login_data, bilanz_data, search_data, TERMS_DICT, URL_DICT
-from main.sql_tools import Tables
+
+from main import logindata
+from .settings import ENGINE_ADRESS, OUTPUT_DIR, SQL_CONNECTION_STR, TERMS_DICT, URL_DICT, bilanz_data, login_data, \
+    search_data
 
 locale.setlocale(locale.LC_ALL, '')
 
@@ -621,3 +626,86 @@ class Crawler:
         crsr.close()
         cnxn.close()
         con.close()
+
+
+class Tables:
+    def __init__(self):
+        self.connection = create_engine(ENGINE_ADRESS, encoding='utf-8').connect()
+        self.sql_connection = pyodbc.connect(SQL_CONNECTION_STR)
+        self.db_cursor = self.sql_connection.cursor()
+
+    @timer
+    def upload_from_dict(self, collection_dict: dict, to_file: bool = False):
+        for table_name, data in collection_dict.items():
+            self.commit(table_name=table_name + 'temp',
+                        data=pd.DataFrame(data),
+                        filename=table_name + '.csv' if to_file else None)
+
+        self.update_tables()
+
+    def update_tables(self):
+        # todo: update_tables() should be redundant, since we only need to append newly
+        # todo: scraped data and keep the format consistent
+        for table_name in filter(lambda content: 'temp' in content, self.table_names):
+            original_table = self.get(table_name=table_name.strip('temp'))
+            temporary_table = self.get(table_name=table_name)
+
+            concat_tables = pd.concat([original_table, temporary_table],
+                                      ignore_index=True,
+                                      sort=False)
+            concat_tables.drop_duplicates(inplace=True)
+
+            self.commit(table_name=table_name.strip('temp'),
+                        data=concat_tables.reset_index(drop=True),
+                        how='replace')
+            self.db_cursor.execute('DROP TABLE ' + table_name)
+        self.sql_connection.commit()
+
+    def get(self, table_name: str):
+        return pd.read_sql(table_name,
+                           self.connection,
+                           index_col='index')
+
+    def commit(self, table_name: str, data: pd.DataFrame,
+               how: str = 'append',
+               filename: str = None):
+        data.to_sql(name=table_name,
+                    con=self.sql_connection,
+                    if_exists=how,
+                    chunksize=10000)
+
+        if filename:
+            os.makedirs(OUTPUT_DIR, exist_ok=True)
+            data.to_csv(path_or_buf=os.path.join(OUTPUT_DIR, filename),
+                        encoding='utf-8')
+
+    def sample(self, table_name: str, n_companies: int, sort_by: str, multi_index: list = None,
+               n: int = 500):
+        """Samples from a given table_name and returns a formatted DataFrame"""
+        df = self.get(table_name=table_name)
+        sampled_companies = np.random.choice(df.FN.unique(), n_companies)
+        df_sampled_sorted = df[df.isin(sampled_companies)].sample(n=n).sort_values(sort_by)
+
+        return df_sampled_sorted.set_index(multi_index) if multi_index else df_sampled_sorted
+
+    def close_connection(self):
+        self.db_cursor.close()
+        self.sql_connection.close()
+        self.connection.close()
+
+    @property
+    def table_names(self):
+        self.db_cursor.execute('Show Tables')
+
+        return list(row[0] for row in self.db_cursor.fetchall())
+
+
+def timer(func):
+    def wrapper(*args, **kwargs):
+        timer_start = time.clock()
+        result = func(*args, **kwargs)
+        timer_end = time.clock()
+        print(f'{func.__name__} executed in {timer_end-timer_start}')
+        return result
+
+    return wrapper
--- run_crawling.py
+++ run_crawling.py
@@ -1,5 +1,5 @@
-from main.settings import DATA_HOOVERS_REVENUE_AT_LEAST_70M
 from main.crawler import Crawler
+from main.settings import DATA_HOOVERS_REVENUE_AT_LEAST_70M
 
 crawler = Crawler()
 crawler.run_from_file(file=DATA_HOOVERS_REVENUE_AT_LEAST_70M,
