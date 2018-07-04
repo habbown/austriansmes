49f661c95c18350f66a40b8651d9fe0d5535368d
meger <egermarcus@gmail.com>
2018-07-04 13:21:01 +0200

    added stemming method

--- main/text.py
+++ main/text.py
@@ -1,16 +1,39 @@
 from difflib import SequenceMatcher
 import pandas as pd
-import nltk
+from nltk.stem.snowball import GermanStemmer
+from tqdm import tqdm
 
 
 class TextClean:
-    @staticmethod
-    def optimize_sentence(words: pd.Series):
-        ds_cleaned = TextClean.remove_noise(series=words)
+    _STEMMER = GermanStemmer()
+
+    def __init__(self, words: pd.Series, optimize: bool = True):
+        self.words = words
+
+        if optimize:
+            self.words = self.optimize()
+
+    def optimize(self):
+        """Runs a series of cleaning, stemming, and optimizing processes"""
+        ds_cleaned = TextClean.remove_noise(series=self.words)
         ds_split = TextClean.split_text(series=ds_cleaned)
         ds_reconstructed = TextClean.reconstruct_sentence(series=ds_split)
 
         return TextClean.remove_stopwords(series=ds_reconstructed)
+
+    # nlp manipulation
+    ##################################
+
+    @staticmethod
+    def spell_correction(series: pd.Series):
+        return
+
+    @classmethod
+    def stem_text(cls, series: pd.Series):
+        return series.apply(lambda x: ' '.join(cls._STEMMER.steam(i) for i in x.split(' ')))
+
+    # string manipulation
+    ##################################
 
     @staticmethod
     def split_text(series: pd.Series):
@@ -26,10 +49,24 @@ class TextClean:
 
     @staticmethod
     def remove_stopwords(series: pd.Series):
-        return
+        return series
 
 
-class TextCluster:
+class TextCluster(TextClean):
+    def group_words(self):
+        unique_terms = self.words.str.lower().unique()
+        term_dict = dict()
+
+        for term in tqdm(iterable=unique_terms,
+                         desc=f'Grouping series ::: '):
+            term_dict[term] = set()
+
+            for sub_term in unique_terms:
+                if sub_term != term:
+                    term_dict[term].update(sub_term)
+
+        return term_dict
+
     @staticmethod
     def match_word_to_iterable(ref: str, iterable):
         return list(TextCluster.get_similarity(ref, word) for word in iterable if ref != word)
